86da71ff96ffe8074fbb8ce4b2bafb1e68e4d960
Rohit Verma <rohit.verma03@nagarro.com>
2021-05-16 18:40:31 +0530

    setting up graphql

---
+++ README.md
@@ -0,0 +1,17 @@
+GraphQL is just a specification like Ecmascript which is implemented by different browsers like Chrome has v8.
+This specification can be implemented by any language.
+
+
+This specification is created by Facebook and you can read it.
+
+We will use graphql-yoga package to use it in out project.
+
+
+// Default exports has no name. You can have only one;
+// Named export has a name. Have as many needed.
+
+const message = 'Some message from myModule.js';
+const location = "Baroda";
+
+
+export {message, location as default};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("hello graphql");
+import {GraphQLServer} from 'graphql-yoga';
+
+// Type definitions a.k.a Application Schema.
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+    }
+`
+
+
+//Resolvers
+
+const resolvers = {
+    Query:{
+        hello(){
+            return 'This is my first query!'
+        },
+        name(){
+            return 'Andrew Mead'
+        }
+    }
+}
+
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+
+server.start(()=>{
+    console.log("The server is up")
+})
