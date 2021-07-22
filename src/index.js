5a426eac1e2c7469adf3c80b3f3cf6df11a05af7
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-22 23:04:16 +0530

    mutation for related data

--- src/index.js
+++ src/index.js
@@ -74,6 +74,9 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email:String!, age:Int): User!
+        createPost(title: String!, body: String!, published:Boolean!, author: ID!): Post!
+        createComment(text: String!, author: ID!, post: ID!): Comment!
+
     }
     
 
@@ -170,6 +173,49 @@ const resolvers = {
             return user
 
 
+        },
+        createPost(parent, args, ctx, info){
+            const userExist = users.some((user)=>{
+                return user.id ==  args.author
+            })
+
+            console.log(users)
+            // console.log(author)
+
+            if(!userExist){
+                throw new Error('User not found')
+            }
+
+            const post = {
+                id:uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            }
+
+            posts.push(post)
+
+            return post
+        },
+        createComment(parent, args, ctx, info){
+            const userExists = users.some((user)=> user.id === args.author)
+            const postExists = posts.some((post)=> post.id === args.post && post.published)
+
+            if(!userExists || !postExists){
+                throw new Error('Unable to find user and postr')
+            }
+
+            const comment = {
+                id: uuidv4(),
+                text: args.text,
+                author: args.author,
+                post: args.post
+            }
+
+            comments.push(comment)
+
+            return comment
         }  
     },
     Post:{
