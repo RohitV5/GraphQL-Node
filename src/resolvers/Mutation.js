9e73213c9aea4fcbe168fe2fbcbaa2a9d975a313
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-24 14:46:22 +0530

    mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -51,6 +51,37 @@ const Mutation = {
 
         return deleteUsers[0]
     },
+    updateUser(parent, args, { db }, info){
+        const {id, data} = args
+        const user = db.users.find((user)=>  user.id = id)
+
+        if(!user) {
+            throw new Error('User not found')
+        }
+        
+        if(typeof data.email === 'string') {
+            const emailTaken = db.users.some((user)=> user.email === data.email)
+
+            if(emailTaken){
+                throw new Error('Email taken')
+            }
+
+            user.email = data.email
+        }
+
+
+        if(typeof data.name === 'string') {
+            user.name = data.name
+        }
+
+
+        if(typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+
+    },
     createPost(parent, args, {db}, info){
         const userExist = db.users.some((user)=>{
             return user.id ==  args.data.author
@@ -82,6 +113,28 @@ const Mutation = {
         
         return deletedPosts[0]
     },
+    updatePost(parent, args, {db}, info){
+        const {id, data} = args
+        const post = db.posts.find((post)=> post.id === id)
+
+        if(!post){
+            throw new Error("Post not found")
+        }
+
+        if(typeof data.title === 'string'){
+            post.title = data.title
+        }
+
+        if(typeof data.body === 'string'){
+            post.body = data.body
+        }
+
+        if(typeof data.published === 'boolean'){
+            post.published = data.published
+        }
+
+        return post
+    },
     createComment(parent, args, {db}, info){
         const userExists = db.users.some((user)=> user.id === args.data.author)
         const postExists = db.posts.some((post)=> post.id === args.data.post && post.published)
@@ -109,6 +162,20 @@ const Mutation = {
         const deletedComments = db.comments.splice(commentIndex, 1)
 
         return deletedComments[0]
+    },
+    updateComment(parent,args,{db},info){
+        const {id, data} = args
+        const comment = db.comments.find((comment)=> comment.id == id)
+
+        if(!comment){
+            throw new Error("Comment not found")
+        }
+
+        if(typeof data.text === 'string'){
+            comment.text = data.text
+        }
+
+        return comment
     }    
 }
 
