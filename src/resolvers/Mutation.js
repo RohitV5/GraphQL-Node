1100e2b21997a8eb424e684599fa85f0f3c30893
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-25 13:02:49 +0530

    comment subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -216,22 +216,35 @@ const Mutation = {
 
         db.comments.push(comment)
 
-        pubsub.publish(`comment ${args.data.post}`, { comment: comment })
+        pubsub.publish(`comment ${args.data.post}`, { 
+            comment: {
+                mutation: 'CREATED',
+                data:comment
+            } 
+        })
 
         return comment
     },
-    deleteComment(parent,args,{db},info){
+    deleteComment(parent,args,{db,pubsub},info){
         const commentIndex = db.comments.findIndex((comment)=>comment.id === args.id)
 
         if(commentIndex == -1){
             throw new Error('Comment does not exist')
         }
 
-        const deletedComments = db.comments.splice(commentIndex, 1)
+        const [deletedComment] = db.comments.splice(commentIndex, 1)
 
-        return deletedComments[0]
+        pubsub.publish(`comment ${deletedComment.post}`, { 
+            comment: {
+                mutation: 'DELETED',
+                data:deletedComment
+            } 
+        })
+
+
+        return deletedComment
     },
-    updateComment(parent,args,{db},info){
+    updateComment(parent,args,{db,pubsub},info){
         const {id, data} = args
         const comment = db.comments.find((comment)=> comment.id == id)
 
@@ -242,6 +255,13 @@ const Mutation = {
         if(typeof data.text === 'string'){
             comment.text = data.text
         }
+
+        pubsub.publish(`comment ${comment.post}`, { 
+            comment: {
+                mutation: 'UPDATED',
+                data:comment
+            } 
+        })
 
         return comment
     }    
